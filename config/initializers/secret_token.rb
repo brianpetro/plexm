566c66fdab3d4b982e8b5961f1c65dec5ca5bdf2
BrianPetro <brian@plexm.com>
2013-01-16 11:41:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plexm::Application.config.secret_token = 'ed397cbe483dc8e33711549b48afa8f2b0a65ad6ae0e50117a92d31ec4d70a8061b96fce3a347c5701c3069312a77af12152b9fbd6ba88e34c152df8496f9cf7'
