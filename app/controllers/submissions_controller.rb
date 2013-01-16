04daf52f4d662ee112865f008b6e745e8d0a9d19
BrianPetro <brian@plexm.com>
2013-01-16 15:19:54 -0500

    Made adjustments for nesting, added bounty and upload variables

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,14 @@
 class SubmissionsController < ApplicationController
-	before_filter :authenticate_user!
+  load_and_authorize_resource
+	before_filter :get_bounty
+	
+	def get_bounty
+		@bounty = Bounty.find(params[:bounty_id])
+	end
   # GET /submissions
   # GET /submissions.json
   def index
-    @submissions = Submission.all
+    @submissions = @bounty.submissions.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,7 +19,7 @@ class SubmissionsController < ApplicationController
   # GET /submissions/1
   # GET /submissions/1.json
   def show
-    @submission = Submission.find(params[:id])
+    @submission = @bounty.submissions.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -25,8 +30,8 @@ class SubmissionsController < ApplicationController
   # GET /submissions/new
   # GET /submissions/new.json
   def new
-    @submission = Submission.new
-
+    @submission = @bounty.submissions.new
+		@uploads = Upload.where(user_id: current_user.id)
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @submission }
@@ -35,17 +40,18 @@ class SubmissionsController < ApplicationController
 
   # GET /submissions/1/edit
   def edit
-    @submission = Submission.find(params[:id])
+    @submission = @bounty.submissions.find(params[:id])
+		@uploads = Upload.where(user_id: current_user.id)
   end
 
   # POST /submissions
   # POST /submissions.json
   def create
-    @submission = Submission.new(params[:submission])
+    @submission = @bounty.submissions.new(params[:submission])
 
     respond_to do |format|
       if @submission.save
-        format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
+        format.html { redirect_to [@bounty, @submission], notice: 'Submission was successfully created.' }
         format.json { render json: @submission, status: :created, location: @submission }
       else
         format.html { render action: "new" }
@@ -57,7 +63,7 @@ class SubmissionsController < ApplicationController
   # PUT /submissions/1
   # PUT /submissions/1.json
   def update
-    @submission = Submission.find(params[:id])
+    @submission = @bounty.submissions.find(params[:id])
 
     respond_to do |format|
       if @submission.update_attributes(params[:submission])
@@ -73,7 +79,7 @@ class SubmissionsController < ApplicationController
   # DELETE /submissions/1
   # DELETE /submissions/1.json
   def destroy
-    @submission = Submission.find(params[:id])
+    @submission = @bounty.submissions.find(params[:id])
     @submission.destroy
 
     respond_to do |format|
