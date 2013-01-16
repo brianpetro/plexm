b6cddd1a5e681ee13f86aa0b05977aa249bbbe0f
BrianPetro <brian@plexm.com>
2013-01-16 17:16:51 -0500

    validate email

--- app/models/prelaunch.rb
+++ app/models/prelaunch.rb
@@ -1,3 +1,7 @@
 class Prelaunch < ActiveRecord::Base
   attr_accessible :email
+
+	validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+	validates_presence_of :email
+	validates_uniqueness_of :email
 end
