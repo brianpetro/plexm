581efb1ba44c819d3e9521d5ec92a6cf4e45d301
BrianPetro <brian@plexm.com>
2013-01-17 17:26:36 -0500

    edit form appropriately to work on bounty show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117155252) do
+ActiveRecord::Schema.define(:version => 20130117205910) do
 
   create_table "bounties", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130117155252) do
     t.integer  "bounty_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "parent_id"
   end
 
   create_table "prelaunches", :force => true do |t|
