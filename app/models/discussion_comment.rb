5bf108ab252d08c2d620410b4609fccad2b14011
BrianPetro <brian@plexm.com>
2013-01-17 18:33:30 -0500

    add user_id to attr acceptable in model

--- app/models/discussion_comment.rb
+++ app/models/discussion_comment.rb
@@ -1,5 +1,5 @@
 class DiscussionComment < ActiveRecord::Base
-  attr_accessible :bounty_id, :comment, :discussion_id
+  attr_accessible :bounty_id, :comment, :discussion_id, :user_id
 	belongs_to :bounty
 	belongs_to :user
 end
