542655d9d8ec6a69a27698ff3bdc4a4207822bfd
BrianPetro <brian@plexm.com>
2013-01-17 21:17:09 -0500

    add attr :agreement and validate_acceptance_of

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,8 +1,10 @@
 class Upload < ActiveRecord::Base
-  attr_accessible :title, :upload, :user_id
+  attr_accessible :title, :upload, :user_id, :agreement
 	belongs_to :user
 	has_many :submissions
 	has_many :bounties, through: :submissions
 
 	mount_uploader :upload, UploadUploader
+
+	validates_acceptance_of :agreement
 end
