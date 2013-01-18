f7c2572c0c41418c80a933940e6cce16ddee68f1
BrianPetro <brian@plexm.com>
2013-01-18 17:33:04 -0500

    add get user id from winner id

--- app/helpers/bounties_helper.rb
+++ app/helpers/bounties_helper.rb
@@ -3,4 +3,9 @@ module BountiesHelper
 		@user = User.find_by_id(uid)
 		return @user.name
 	end
+
+	def get_user_id_from_upload_id(uid)
+		@upload = Upload.find_by_id(uid)
+		return @upload.user_id
+	end
 end
