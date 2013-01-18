ff83e7cfde96551d62b431600cfe6f48283705cf
BrianPetro <brian@plexm.com>
2013-01-17 23:25:31 -0500

    make :paid attr

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,5 +1,5 @@
 class Bounty < ActiveRecord::Base
-  attr_accessible :body, :bounty, :title, :user_id
+  attr_accessible :body, :bounty, :title, :user_id, :paid
 	belongs_to :user
 	has_many :submissions
 	has_many :uploads, through: :submissions
