ca9baf8465d4f612faa05b615f047926d270cfd6
BrianPetro <brian@plexm.com>
2013-01-18 16:47:52 -0500

    accept_nested_attributes_for :winner

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,5 +1,6 @@
 class Bounty < ActiveRecord::Base
   attr_accessible :body, :bounty, :title, :user_id, :paid
+  accepts_nested_attributes_for :uploads
 	belongs_to :user
 	has_many :submissions
 	has_many :uploads, through: :submissions
