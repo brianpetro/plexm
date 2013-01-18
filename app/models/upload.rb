49a7367aea7df18e98da16c01ac6a4442196dbbf
BrianPetro <brian@plexm.com>
2013-01-17 21:29:05 -0500

    add validations to upload form

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,8 +3,9 @@ class Upload < ActiveRecord::Base
 	belongs_to :user
 	has_many :submissions
 	has_many :bounties, through: :submissions
-
 	mount_uploader :upload, UploadUploader
-
+	validates_presence_of :title
+	validates_presence_of :upload
+	validates_presence_of :artist
 	validates_acceptance_of :agreement
 end
