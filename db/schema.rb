b968f8b2a626fbac8373ff85ed7e754b7e582b97
BrianPetro <brian@plexm.com>
2013-01-24 20:33:10 -0500

    rails g model tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124014325) do
+ActiveRecord::Schema.define(:version => 20130125012605) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -83,6 +83,12 @@ ActiveRecord::Schema.define(:version => 20130124014325) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "uploads", :force => true do |t|
