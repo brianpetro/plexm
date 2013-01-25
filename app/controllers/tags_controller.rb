edd9786c106cd248b88faafdca7664e7c781a391
BrianPetro <brian@plexm.com>
2013-01-24 22:46:30 -0500

    name change error causing token box to not work

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
-class TagController < ApplicationController
+class TagsController < ApplicationController
 	def index
 		@tags = Tag.order(:name)
 		respond_to do |format|
 			format.html
-			format.json { render json: @tags.tokens(params[:q}) }
+			format.json { render json: @tags.tokens(params[:q]) }
 		end
 	end
 
