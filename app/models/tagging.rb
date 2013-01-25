34cfabcff00a54787f6eb7280b8481c7846e9c8c
BrianPetro <brian@plexm.com>
2013-01-24 20:37:54 -0500

    generate model Tagging

---
+++ app/models/tagging.rb
@@ -0,0 +1,3 @@
+class Tagging < ActiveRecord::Base
+  attr_accessible :bounty_id, :tag_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125012605) do
+ActiveRecord::Schema.define(:version => 20130125013612) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(:version => 20130125012605) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "bounty_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "tags", :force => true do |t|
