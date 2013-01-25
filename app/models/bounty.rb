2301baa9728ac8f4233f488d208014c40d4e378b
BrianPetro <brian@plexm.com>
2013-01-24 21:05:48 -0500

    accept tag_ids

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,5 +1,5 @@
 class Bounty < ActiveRecord::Base
-  attr_accessible :body, :bounty, :title, :user_id, :paid, :winner, :closed
+  attr_accessible :body, :bounty, :title, :user_id, :paid, :winner, :closed, :tag_ids
 	belongs_to :user
 	has_many :submissions
 	has_many :uploads, through: :submissions
@@ -7,5 +7,5 @@ class Bounty < ActiveRecord::Base
 	has_many :discussion_comments
 
 	has_many :taggings
-	has_many :tags, through: :taggings
+	has_many :tags, through: :taggings, uniq: true
 end
